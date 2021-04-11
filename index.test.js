743615b9eafba9c05afa39019af1ed37e05eb2a1
Gabriel Rufino <contato@gabrielrufino.com>
2021-04-11 15:02:15 -0300

    Tests

---
+++ index.test.js
@@ -0,0 +1,114 @@
+const { deepStrictEqual, throws } = require('assert')
+
+const sioninver = require('.')
+
+function log(string) {
+  process.stdout.write(string)
+}
+
+{
+  log('Should invert an object correctly')
+
+  const object = {
+    '201': 'Created',
+    '404': 'Not Found',
+    '500': 'Server Error'
+  }
+
+  const expected = {
+    'Created': '201',
+    'Not Found': '404',
+    'Server Error': '500'
+  }
+
+  const inveterd = sioninver(object)
+  deepStrictEqual(inveterd, expected)
+
+  log(' [Pass]\n\r')
+}
+
+{
+  log('Should throw an error when does not receives the parameter')
+
+  throws(
+    () => {
+      const inverted = sioninver()
+    },
+    {
+      name: 'Error',
+      message: 'Parameter is empty'
+    }
+  )
+
+  log(' [Pass]\n\r')
+}
+
+{
+  log('Should throw an error when receives an non object parameter')
+
+  throws(
+    () => {
+      const inverted = sioninver(true)
+    },
+    {
+      name: 'TypeError',
+      message: 'Parameter is not an object'
+    }
+  )
+
+  throws(
+    () => {
+      const inverted = sioninver(1)
+    },
+    {
+      name: 'TypeError',
+      message: 'Parameter is not an object'
+    }
+  )
+
+  throws(
+    () => {
+      const inverted = sioninver('Gabriel Rufino')
+    },
+    {
+      name: 'TypeError',
+      message: 'Parameter is not an object'
+    }
+  )
+
+  throws(
+    () => {
+      const inverted = sioninver(['Apple', 'Banana', 'Orange'])
+    },
+    {
+      name: 'TypeError',
+      message: 'Parameter is not an object'
+    }
+  )
+
+  log(' [Pass]\n\r')
+}
+
+{
+  log('Should throw an error when receives non unique values')
+
+  const object = {
+    '001': 'Gabriel Rufino',
+    '002': 'Erick Wendel',
+    '003': 'Elon Musk',
+    '004': 'Lewis Hamilton',
+    '005': 'Gabriel Rufino'
+  }
+
+  throws(
+    () => {
+      const inverted = sioninver(object)
+    },
+    {
+      name: 'Error',
+      message: 'There are repetitions of values'
+    }
+  )
+
+  log(' [Pass]\n\r')
+}
